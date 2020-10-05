8ae18866d1ad20732d71b22b2bc861b5d9ad4832
RHalai-Engin <Rajkumarhalai0@gmail.com>
2020-10-05 17:53:20 -0500

    initial commit

---
+++ public/Scripts/app.js
@@ -0,0 +1,9 @@
+//IIFE - Immediately Invoke Function Express
+(function(){
+    function Start()
+    {
+        console.log("App Started...");
+    }
+
+    window.addEventListener("load", Start);
+})();
