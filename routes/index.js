80e6c6f6600ad2995fc330f985dddd11ce322f8c
RHalai-Engin <Rajkumarhalai0@gmail.com>
2020-10-06 18:57:21 -0500

    add info about me

--- routes/index.js
+++ routes/index.js
@@ -8,27 +8,27 @@ router.get('/', function(req, res, next) {
 
 /* GET home page. */
 router.get('/home', function(req, res, next) {
-  res.render('index', { title: 'Home',});
+  res.render('index', { title: 'Home'});
 });
 
 /* GET About Us page. */
 router.get('/about', function(req, res, next) {
-  res.render('index', { title: 'About',});
+  res.render('index', { title: 'About'});
 });
 
 /* GET Products page. */
 router.get('/products', function(req, res, next) {
-  res.render('index', { title: 'Products',});
+  res.render('index', { title: 'Products'});
 });
 
 /* GET Services page. */
 router.get('/services', function(req, res, next) {
-  res.render('index', { title: 'Services',});
+  res.render('index', { title: 'Services'});
 });
 
 /* GET Contact Us page. */
 router.get('/contact', function(req, res, next) {
-  res.render('index', { title: 'Contact',});
+  res.render('index', { title: 'Contact'});
 });
 
 module.exports = router;
