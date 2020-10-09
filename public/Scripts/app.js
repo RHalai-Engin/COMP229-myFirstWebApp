e6fbb28d91dedbb57922ae43fc488641e0951a85
RHalai-Engin <Rajkumarhalai0@gmail.com>
2020-10-09 04:32:12 -0500

    add form to contact page

--- public/Scripts/app.js
+++ public/Scripts/app.js
@@ -3,6 +3,26 @@
     function Start()
     {
         console.log("App Started...");
+
+        if (document.title == "Contact")
+        {
+            let sendButton = document.getElementById("sendButton");
+            let cancelButton = document.getElementById("cancelButton");
+
+            sendButton.addEventListener("click", (event) => {
+                event.preventDefault();
+            });
+
+            cancelButton.addEventListener("click", (event) => {
+                event.preventDefault();
+                if(confirm("Are you sure?"))
+                {
+                    location.href = "/home";
+                }
+            });
+
+
+        }
     }
 
     window.addEventListener("load", Start);
